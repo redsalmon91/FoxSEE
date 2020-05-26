1cbad76069053d079b23a23ed5dd1834dd11ca9a
Zixiao Han <andyzx.han@aliyun.com>
2020-05-26 21:06:59 +0800

    v3.3.4

--- src/def.rs
+++ src/def.rs
@@ -3,7 +3,7 @@
  */
 
 pub static ENGINE_NAME: &str = "FoxSEE";
-pub static VERSION: &str = "3.3.3T";
+pub static VERSION: &str = "3.3.4";
 pub static AUTHOR: &str = "Zixiao Han";
 
 pub const DEFAULT_HASH_SIZE_MB: usize = 64;
