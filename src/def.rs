37331a4f86aa2717089657cde332baace0b8e129
Zixiao Han <andyzx.han@aliyun.com>
2020-07-21 23:49:30 +0800

    Tuned PST for pawns

--- src/def.rs
+++ src/def.rs
@@ -3,7 +3,7 @@
  */
 
 pub static ENGINE_NAME: &str = "FoxSEE";
-pub static VERSION: &str = "5.0.1";
+pub static VERSION: &str = "5.0.2";
 pub static AUTHOR: &str = "Zixiao Han";
 
 pub const DEFAULT_HASH_SIZE_MB: usize = 64;
