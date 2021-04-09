d08eb36acd6c13ca67fbaf63d0cda8502d859ec7
Andy Han <andyzx.han@aliyun.com>
2021-04-09 13:13:58 +0800

    bump version to 7.2

--- src/def.rs
+++ src/def.rs
@@ -3,7 +3,7 @@
  */
 
 pub static ENGINE_NAME: &str = "FoxSEE";
-pub static VERSION: &str = "7.0.0";
+pub static VERSION: &str = "7.2.0";
 pub static AUTHOR: &str = "Zixiao Han";
 
 pub const DEFAULT_HASH_SIZE_MB: usize = 128;
