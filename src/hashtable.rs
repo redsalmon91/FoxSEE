7945979b056a5b90728bb18c262439c43a345c95
Zixiao Han <andyzx.han@qq.com>
2021-04-14 17:18:13 +0800

    real fix for the hashmov bug

--- src/hashtable.rs
+++ src/hashtable.rs
@@ -98,30 +98,21 @@ impl DepthPreferredHashTable {
                     if depth > entry.exact_depth {
                         entry.exact_score = score;
                         entry.exact_depth = depth;
-
-                        if depth >= entry.depth {
-                            entry.mov = mov;
-                        }
+                        entry.mov = mov;
                     }
                 },
                 HASH_TYPE_BETA => {
                     if depth > entry.lb_depth || (depth == entry.lb_depth && score > entry.lb_score) {
                         entry.lb_score = score;
                         entry.lb_depth = depth;
-
-                        if depth > entry.depth {
-                            entry.mov = mov;
-                        }
+                        entry.mov = mov;
                     }
                 },
                 HASH_TYPE_ALPHA => {
                     if depth > entry.ub_depth || (depth == entry.ub_depth && score < entry.ub_score) {
                         entry.ub_score = score;
                         entry.ub_depth = depth;
-
-                        if depth > entry.depth {
-                            entry.mov = mov;
-                        }
+                        entry.mov = mov;
                     }
                 },
                 _ => {},
