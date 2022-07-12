7d6cee28a65e95ce9273d3c71dd4e9a6ccf00cc7
Andy Han <andyzx.han@qq.com>
2022-07-12 17:45:15 +0800

    Change display version to v8

--- src/def.rs
+++ src/def.rs
@@ -3,7 +3,7 @@
  */
 
 pub static ENGINE_NAME: &str = "FoxSEE";
-pub static VERSION: &str = "8.0.0";
+pub static VERSION: &str = "v8";
 pub static AUTHOR: &str = "Zixiao Han";
 
 pub const DEFAULT_HASH_SIZE_MB: usize = 128;
